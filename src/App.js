32d841d3a0991de935480d8e7367ffc61658874a
MarinaAlmeida20 <marinabrito.ma@gmail.com>
2022-04-22 20:48:39 +0100

    level 1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+// import CountryData from "./data/countriesAll.json";
+import "./App.css";
 
 function App() {
+  const [country, setCountry] = useState(null);
+
+  useEffect(() => {
+    fetch(`https://restcountries.com/v2/all`)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        setCountry(data);
+      });
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div>
+        {country &&
+          country.map((card, index) => {
+            const { flag, name, population, region, capital } = card;
+
+            let populationNumber = population
+              .toString()
+              .replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1.");
+
+            return (
+              <div className="main">
+                <div className="cards" key={index}>
+                  <div className="cards_item">
+                    <div className="card">
+                      <img src={flag} alt="flag" />
+                      <div className="card_container">
+                        <h4>{name}</h4>
+                        <p>
+                          <b>Population:</b> {populationNumber}
+                        </p>
+                        <p>
+                          <b>Region:</b> {region}
+                        </p>
+                        <p>
+                          <b>Capital:</b> {capital}
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            );
+          })}
+      </div>
+      <footer>
+        <h3 className="made_by">Made with ♡ by Marina Almeida 2022</h3>
+      </footer>
+    </>
   );
 }
 
